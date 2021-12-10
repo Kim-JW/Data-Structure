b8708b43ac61ca1e698524952e9032bbe22972db
BIT <BIT@DESKTOP-CJGT332>
2021-12-10 11:38:16 +0900

    Anonymous inner class

--- DataStructure/src/com/java/innerclass/MemberInnerTest.java
+++ DataStructure/src/com/java/innerclass/MemberInnerTest.java
@@ -4,23 +4,43 @@ package com.java.innerclass;
 
 // 숙제 이클립스 단축키 정리
 
+//class Outter {
+//private int num = 10;
+//static int num2 = 3;
+//
+//public void output() {
+//	class Inner {
+//		public void disp() {
+//			System.out.println(num);
+//			System.out.println(num2);
+//		}
+//	}
+//	
+//	Inner in = new Inner();
+//	in.disp();
+//}
+//
+//}
+
+abstract class Abs {
+	abstract void disp(); // "disp"
+}
 
 class Outter {
 	private int num = 10;
-	static int num2 = 3;
+	private static int num2 = 3;
 	
 	public void output() {
-		class Inner {
-			public void disp() {
+		new Abs() {
+
+			@Override
+			void disp() {
 				System.out.println(num);
 				System.out.println(num2);
+				
 			}
-		}
-		
-		Inner in = new Inner();
-		in.disp();
+		}.disp();
 	}
-	
 }
 
 public class MemberInnerTest {
@@ -37,6 +57,8 @@ public class MemberInnerTest {
 		
 		Outter out = new Outter();
 		out.output();
+		
+	
 
 	}
 
